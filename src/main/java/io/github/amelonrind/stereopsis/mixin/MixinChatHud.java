3a0decaee779d1b70eaa1d6b6bcc8f3ea098c4c7
aMelonRind <kong49123@gmail.com>
2024-02-25 10:35:27 +0800

    fixed a bug where the chat may be rendered twice when changing chat width

--- src/main/java/io/github/amelonrind/stereopsis/mixin/MixinChatHud.java
+++ src/main/java/io/github/amelonrind/stereopsis/mixin/MixinChatHud.java
@@ -1,6 +1,5 @@
 package io.github.amelonrind.stereopsis.mixin;
 
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.ChatHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static io.github.amelonrind.stereopsis.Stereopsis.enabled;
+import static io.github.amelonrind.stereopsis.Stereopsis.mc;
 
 @Mixin(ChatHud.class)
 public abstract class MixinChatHud {
@@ -19,24 +19,30 @@ public abstract class MixinChatHud {
 
     @Shadow public abstract void reset();
 
-    @Unique private DrawContext context = null;
-    @Unique private int lastWidth = 1;
+    @Unique private int lastWidth = 0;
+    @Unique private boolean notRefreshing = true;
 
-    @Inject(method = "render", at = @At("HEAD"))
-    public void updateContext(DrawContext context, int currentTick, int mouseX, int mouseY, CallbackInfo ci) {
-        if (enabled) this.context = context;
+    @Inject(method = "refresh", at = @At("HEAD"))
+    private void onRefresh(CallbackInfo ci) {
+        notRefreshing = false;
+    }
+
+    @Inject(method = "refresh", at = @At("TAIL"))
+    private void afterRefresh(CallbackInfo ci) {
+        notRefreshing = true;
     }
 
     @Inject(method = "getWidth()I", at = @At("RETURN"), cancellable = true)
-    public void modifyWidth(CallbackInfoReturnable<Integer> cir) {
+    private void modifyWidth(CallbackInfoReturnable<Integer> cir) {
         int width = cir.getReturnValueI();
-        if (enabled && !isChatFocused() && context != null) {
-            int max = context.getScaledWindowWidth() / 3;
+        if (enabled && !isChatFocused()) {
+            int max = mc.getWindow().getScaledWidth() / 3;
             if (width > max) cir.setReturnValue(width = max);
         }
         if (width != lastWidth) {
             lastWidth = width;
-            reset();
+            if (notRefreshing) reset();
+            else notRefreshing = true; // this is safe... right?
         }
     }
 
